5d225560b834fa655d7195ed5edd40f12b2c0be7
Gabriel Pickl <peacemotion@gmail.com>
2017-08-23 22:57:39 +0200

    Added Ga game engine.

--- src/app/main.js
+++ src/app/main.js
@@ -1,69 +1,43 @@
+import GA from "./ga.js"
 
-const canvas = document.getElementById("canvas")
+import person_url from "../sprites/person.png"
 
-const ctx = canvas.getContext("2d")
 
-const image = new Image()
-image.src = require("../sprites/person.png")
+function loadImage(fn, ga, name, data) {
+  const img = new Image()
 
-const map = new Image()
-map.src = require("../sprites/map.png")
+  img.src = data
 
-const LEFT = {}
-const RIGHT = {}
-const UP = {}
-const DOWN = {}
-const STANDING = {}
-
-const speed = 50
-
-let posX = 160, posY = 120
-let direction = STANDING
-let lastTime = 0
-
-document.addEventListener("keydown", (event) => {
-  if(event.keyCode == 37) //LEFT
-    direction = LEFT
-  if(event.keyCode == 38) //UP
-    direction = UP
-  if(event.keyCode == 39) //RIGHT
-    direction = RIGHT
-  if(event.keyCode == 40) //DOWN
-    direction = DOWN
-})
-
-document.addEventListener("keyup", (event) => {
-  if(event.keyCode >= 37 && event.keyCode <= 40)
-    direction = STANDING
-})
-
-function render() {
-  ctx.fillStyle = "black"
-  ctx.fillRect(0, 0, 320, 240)
-  ctx.drawImage(image, Math.round(posX)-16, Math.round(posY)-16)
+  img.addEventListener("load", () => {
+    ga.assets[name] = {
+      source: img,
+      frame: {
+        x: 0,
+        y: 0,
+        w: img.width,
+        h: img.height
+      }
+    }
+    fn()
+  })
 }
 
-function step(timestamp) {
-  const delta = (lastTime == 0 ? 0 : timestamp - lastTime) / 1000
-  lastTime = timestamp
+let gameScene
 
-  switch(direction) {
-    case LEFT:
-      posX -= delta*speed
-      break
-    case RIGHT:
-      posX += delta*speed
-      break
-    case UP:
-      posY -= delta*speed
-      break
-    case DOWN:
-      posY += delta*speed
-      break
-  }
-
-  render()
-  window.requestAnimationFrame(step)
+function step() {
 }
 
-window.requestAnimationFrame(step)
+function setup() {
+  gameScene = ga.group()
+  const personSprite = ga.sprite("person.png")
+  //gameScene.addChild(personSprite)
+
+  ga.state = step
+}
+
+const ga = GA.create(320, 240, setup
+)
+
+loadImage(() => {
+  ga.start()
+}, ga, "person.png", person_url)
